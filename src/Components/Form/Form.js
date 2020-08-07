ab1298f2528c8181ead192a5abe5d19197f45b80
Ryan Milne <ryanmilne101@gmail.com>
2020-08-07 11:02:48 -0600

    P1 S1

--- src/Components/Form/Form.js
+++ src/Components/Form/Form.js
@@ -1,10 +1,57 @@
 import React, {Component} from 'react'
 
 class Form extends Component {
+    constructor(){
+        super()
+        this.state = {
+           name: '',
+           price: 0,
+           img: '' 
+        }
+    }
+
+    handleChange = (e) => {
+        this.setState({
+          [e.target.name]: e.target.value,
+        });
+      };
+
+
+
     render(){
+        const {name,price,img}= this.state
         return (
             <div>
-                <h1>Form Component</h1>
+                <input
+                onChange={(e) => this.handleChange(e)}
+                name = 'name'
+                placeholder = '...name'
+                value = {name}
+                type = 'text'
+
+                />
+                <input
+                onChange={(e) => this.handleChange(e)}
+                name = 'price'
+                placeholder = '...price'
+                value = {price}
+                type = 'text'
+
+                />
+                <input
+                onChange={(e) => this.handleChange(e)}
+                name = 'img'
+                placeholder = '...img'
+                value = {img}
+                type = 'text'
+
+                />
+                <button
+                onClick={(e) => {
+                    this.setState({name: '', price: 0, img: ''})
+                }}
+                >Cancel</button>
+                <button>Add</button>
             </div>
         )
     }
